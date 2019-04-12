644efdaa9107e8dad162a2e51b5fde49614e7589
visualstudio <visualstudio@122-PC33>
2019-04-12 17:43:53 -0400

    first try

--- c0730319/Program.cs
+++ c0730319/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace c0730319
 {
-    class Program
+    class Program2
     {
-        static void Main(string[] args)
+        string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "1", "T", "k", "1",
+            "m", "n", "o", "p", "q", "/", "s", "t", "u", "V", "W", "X", "Y", "z" };
+
+        public static void Main()
         {
+
+            Program2 orange = new Program2();
+            Console.WriteLine(orange.Gematria("universe"));
+        }
+
+        public int Gematria(string word)
+        {
+            int GematriaValue = 0;
+            for (int i = 0; i < word.Length; i++)
+            { GematriaValue += LetterValue(word[i].ToString()); }
+            return GematriaValue;
+        }
+
+        public int LetterValue(string letter) {
+            int x = 0;
+            while (Letters[x] != letter)
+            {
+                if (Letters[x++] == letter) return x;
+            }
+            return -1;
         }
     }
 }
