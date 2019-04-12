dbd73ffe68f74d69738073241e68c0cca6e918a8
visualstudio <visualstudio@122-PC33>
2019-04-12 18:06:52 -0400

    gemataria done

--- c0730319/Program.cs
+++ c0730319/Program.cs
@@ -8,31 +8,34 @@ namespace c0730319
 {
     class Program2
     {
-        string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "1", "T", "k", "1",
-            "m", "n", "o", "p", "q", "/", "s", "t", "u", "V", "W", "X", "Y", "z" };
+        string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
         public static void Main()
         {
 
             Program2 orange = new Program2();
             Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
         }
-
         public int Gematria(string word)
         {
             int GematriaValue = 0;
             for (int i = 0; i < word.Length; i++)
-            { GematriaValue += LetterValue(word[i].ToString()); }
+            {
+                GematriaValue += LetterValue(word[i].ToString());
+            }
+
             return GematriaValue;
         }
-
-        public int LetterValue(string letter) {
+        public int LetterValue(string letter)
+        {
             int x = 0;
             while (Letters[x] != letter)
             {
-                if (Letters[x++] == letter) return x;
+                if (Letters[x++] == letter)
+                    return -1;
             }
-            return -1;
+            return x + 1;
         }
     }
 }
